d2d4cdae543fd3c1008605e68b1b66764fd3df03
matthew040519 <63332916+matthew040519@users.noreply.github.com>
2023-12-15 00:55:58 +0800

    role check

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-        'restrictRole' => \App\Http\Middleware\CheckRole::class,
+        'restrictRole' => \App\Http\Middleware\checkRole::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
